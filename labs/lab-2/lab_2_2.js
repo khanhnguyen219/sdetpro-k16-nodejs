15b129b4418622bea4af6b604e929b6073f92550
khanhnguyen <nguyentrongkhanh.219@gmail.com>
2025-01-14 01:13:54 +0700

    update lab_2_2.js

--- labs/lab-2/lab_2_2.js
+++ labs/lab-2/lab_2_2.js
@@ -4,8 +4,12 @@ const readline = require('readline-sync');
 
 let numberInput = Number(readline.question("Enter the number:  "));
 
-if(numberInput % 2 == 0){
-    console.log(`${numberInput} is an even number`);
-}else {
-    console.log(`${numberInput} is an odd number`);
-}
+// if(numberInput % 2 == 0){
+//     console.log(`${numberInput} is an even number`);
+// }else {
+//     console.log(`${numberInput} is an odd number`);
+// }
+
+let msg = numberInput % 2 ==0 ? `${numberInput} is an even number` : `${numberInput} is an odd number`;
+console.log(msg);
+
