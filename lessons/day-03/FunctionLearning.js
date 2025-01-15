194800caf874ffdecf1188bc3f1e269d21c50f52
Khanh.Nguyen4 <khanh.nguyen4@sts.saigontechnology>
2025-01-15 17:49:01 +0700

    day-03 FunctionLearning commit

---
+++ lessons/day-03/FunctionLearning.js
@@ -0,0 +1,54 @@
+// DRY: Don't Repeat Yourself
+// Single Reponsibility: Tính đơn nhiệm
+
+// function name: 
+// camelCase 
+// Declare a function
+const readline = require('readline-sync');
+
+// Method 01: [ Function Declaration ] method hoisting
+let userInputNum = getUSerInputNum();
+let inputNumber = convertToNumber(userInputNum);
+eavluaEvenOrOddNumber(inputNumber);
+
+function getUSerInputNum(){
+    let numberInput = Number(readline.question("Enter the number:  "));
+    return numberInput;
+}
+
+function convertToNumber(str ){
+    let number = Number(str);
+    if(isNaN(number)){
+        console.log("Không phải kiểu number, nhập lại");
+        return null;
+    }
+    return number;
+}
+
+function eavluaEvenOrOddNumber(number){
+    if(number === null){
+        return;
+    }else if(number % 2 ===0 ){
+        console.log(`${number} is even number`);
+        
+    }else{
+        console.log(`${number} is odd number`);
+        
+    }
+}
+
+
+
+
+
+// console.log(returnValue);
+// function functionName(){
+//     return 1;
+// }
+
+// // Method 02: [ Function Expression ] method | No hoisting
+// let sum = addNumber(1, 2);
+// const addNumber = function(firstNumber, secondNumber){
+    
+//     return firstNumber + secondNumber;
+// }
