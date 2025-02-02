96b7dc3de2cb76abe46db7f20c54646c6f2290f4
Khanh.Nguyen4 <khanh.nguyen4@sts.saigontechnology>
2025-02-02 21:59:58 +0700

    day-04

--- lessons/day-03/FunctionLearning.js
+++ lessons/day-03/FunctionLearning.js
@@ -8,26 +8,27 @@ const readline = require('readline-sync');
 
 // Method 01: [ Function Declaration ] method hoisting
 let userInputNum = getUSerInputNum();
-let inputNumber = convertToNumber(userInputNum);
-eavluaEvenOrOddNumber(inputNumber);
+// let inputNumber = convertToNumber(userInputNum);
+eavluaEvenOrOddNumber(userInputNum);
 
-function getUSerInputNum(){
+function getUSerInputNum(number){
     let numberInput = Number(readline.question("Enter the number:  "));
-    return numberInput;
+    console.log(numberInput);
+    return numberInput; 
 }
 
-function convertToNumber(str ){
-    let number = Number(str);
-    if(isNaN(number)){
-        console.log("Không phải kiểu number, nhập lại");
-        return null;
-    }
-    return number;
-}
+// function convertToNumber(str ){
+//     let number = Number(str);
+//     if(isNaN(number)){
+//         console.log("Không phải kiểu number, nhập lại");
+//         return null;
+//     }
+//     return number;
+// }
 
 function eavluaEvenOrOddNumber(number){
     if(number === null){
-        return;
+        return null;
     }else if(number % 2 ===0 ){
         console.log(`${number} is even number`);
         
