fbd35fe00df2acbf809a35d991ef3f7531995627
khanhnguyen <nguyentrongkhanh.219@gmail.com>
2025-02-02 22:07:45 +0700

    Add FunctionLearning.js

---
+++ lessons/day-03/FunctionLearning.js
@@ -0,0 +1,8 @@
+// DRY: Don't Repeat Yourself
+// Single Responsibility: Tính đơn nhiệm
+// Clean Code
+
+// Declaration 
+
+// Method 01: [ Function Declaration ] method | Hoisting
+// Method 02: [ Function Expression ] method | No Hoisting
