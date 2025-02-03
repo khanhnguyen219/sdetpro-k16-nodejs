dabab9570de3ae851fdff95c7db15a1c7d2ddcff
khanhnguyen <nguyentrongkhanh.219@gmail.com>
2025-02-03 12:06:21 +0700

    Update changed

---
+++ labs/UtilMethod.js
@@ -0,0 +1,38 @@
+function findMinMax(arr) {
+    let minValue = arr[0];
+    let maxValue = arr[0];
+
+    for (let index = 1; index < arr.length; index++) {
+        if (arr[index] < minValue) {
+            minValue = arr[index];
+        }
+
+        if (arr[index] > maxValue) {
+            maxValue = arr[index];
+        }
+
+
+    }
+    return { min: minValue, max: maxValue }
+}
+
+function countEvenAndOddNum(arr){
+    let evenNum = 0;
+    let oddNum = 0;
+    for (let index = 0; index < arr.length; index++) {
+        if (arr[index] % 2 ===0) {
+            evenNum++;
+        }
+
+        if (arr[index] % 2 !==0) {
+            oddNum++;
+        }
+        
+    }
+    return{even: evenNum, odd: oddNum};
+}
+
+module.exports = {
+    findMinMax,
+    countEvenAndOddNum
+}
