07b20024dfff5e133261baccfc17aed4cc1f99d4
khanhnguyen <nguyentrongkhanh.219@gmail.com>
2025-02-10 08:20:58 +0700

    Update day07

---
+++ lessons/day-07/SwitchCaseStatement.js
@@ -0,0 +1,15 @@
+const readline = require('readline-sync');
+
+let userNum = getUserNumber();
+if (userNum === 2) {
+    
+}else if (userNum === 3) {
+    console.log();
+    
+}{
+
+}
+
+function getUserNumber() {
+    return Number(readline.question('Please enter your number: '))
+}
