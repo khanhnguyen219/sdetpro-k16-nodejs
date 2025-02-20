17c9e7d385e491bd2873383382c234c178d5af4c
khanhnguyen <nguyentrongkhanh.219@gmail.com>
2025-02-20 09:30:25 +0700

    lab4 update

--- labs/lab-4/Lab_4.js
+++ labs/lab-4/Lab_4.js
@@ -1,16 +1,18 @@
 const readline = require('readline-sync');
 
-// DB
+// in-memory DB
 const account1 = {
     accountName: 'Teo',
-    accountNumber: String,
-    rountingNumber: String,
-    balance: String,
+    accountNumber: '123345345',
+    rountingNumber: '123',
+    balance: 5000
 };
 
 const account2 = JSON.parse(JSON.stringify(account1));
 account2.accountName = 'Ti';
+account2.accountNumber = '98761253';
 
+const accounts = [account1, account2];
 // Program interface
 bankApp();
 
@@ -27,10 +29,8 @@ function bankApp() {
                 if (foundAccount.accountNumber) {
                     const { accountName, balance } = foundAccount;
                     console.log(`accountName: ${accountName}, balance: ${balance}`);
-
                 } else {
                     console.log('Account not found');
-
                 }
 
                 break;
@@ -42,7 +42,6 @@ function bankApp() {
             default:
                 console.log('Wrong option!\n');
                 break;
-
         }
         console.log('See you next time!');
 
@@ -65,5 +64,43 @@ function getUserOption() {
 }
 
 function findAccount() {
-    
+    let foundAccount = {};
+    const inputAccountNumber = readline.question('Please enter account number: ');
+    for (const account of accounts) {
+        if(account.accountNumber === inputAccountNumber){
+            foundAccount = account;
+            break;
+        }
+    }
+    // if account found, reassign found account for the found one
+    return foundAccount;
+}
+
+// Readability, extendable, maintainable -- reliable
+
+function updateBalance() {
+    const MAX_ALLOWED_ATTEMP = 3;
+    let attemps = 0;
+    do {
+      const foundAccount = findAccount();
+      const isNotFound = foundAccount.accountNumber === undefined;  
+      if(isNotFound){
+        console.log(`Attemp time: ${attemps + 1}`);
+        
+        attemps++
+      }else{
+        // Logic to update balance and exist the function
+        const{balance} = foundAccount;
+        console.log(`Your balance is : ${balance}`);
+        const expectedWithdrawMoney = Number(readline.question('Please input withdraw amount: '));
+        if(expectedWithdrawMoney > balance){
+            console.log('Insufficient balance!');
+            
+        }else{
+            console.log('Succeed!');
+            foundAccount.balance = balance - expectedWithdrawMoney;
+            attemps = MAX_ALLOWED_ATTEMP;
+        }
+      }
+    } while(attemps < MAX_ALLOWED_ATTEMP)
 }
